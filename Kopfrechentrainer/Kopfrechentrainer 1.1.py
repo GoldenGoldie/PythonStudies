3fcd3b9ecfffcc0b7ab5bbe302d601284982b25a
GoldenGoldie <136335517+GoldenGoldie@users.noreply.github.com>
2023-06-16 16:32:34 +0200

    added new mode

--- Kopfrechentrainer/Kopfrechentrainer 1.1.py
+++ Kopfrechentrainer/Kopfrechentrainer 1.1.py
@@ -3,41 +3,79 @@ import random
 print("what do you want to learn?")
 
 while(True):
+
+    mode = (input("Do you want the easy or hard mode?"))
+
     calculation = (input("Show me the calculation symbol: "))
     
     control_1 = False
-
+                               
     if calculation == "end":
-        break
+           break
 
-    if calculation == "*":
-        number_1 = random.randrange(1,10)
-        number_2 = random.randrange(1,20)
-        calculate = (number_1* number_2)
-        print (f" { number_2 } * {number_1}")
-        control_1 = True   
+    if mode == "easy":
+     
+        if calculation == "*":
+            number_1 = random.randrange(1,10)
+            number_2 = random.randrange(1,20)
+            calculate = (number_1* number_2)
+            print (f" { number_2 } * {number_1}")
+            control_1 = True   
 
-    if calculation == "+":
-        number_1 = random.randrange(20,150)
-        number_2 = random.randrange(20,150)
-        calculate = (number_1 + number_2)
-        print(f"{number_1} + {number_2}")
-        control_1 = True
+        if calculation == "+":
+            number_1 = random.randrange(20,150)
+            number_2 = random.randrange(20,150)
+            calculate = (number_1 + number_2)
+            print(f"{number_1} + {number_2}")
+            control_1 = True
 
-    if calculation == "-":
-        number_1 = random.randrange(20,150)
-        number_2 = random.randrange(20,75)
-        calculate = (number_1 - number_2 )
-        print(f"{number_1} - {number_2}")
-        control_1 = True
+        if calculation == "-":
+            number_1 = random.randrange(20,150)
+            number_2 = random.randrange(20,75)
+            calculate = (number_1 - number_2 )
+            print(f"{number_1} - {number_2}")
+            control_1 = True
 
-    if calculation == "/":
-        number_1 = random.randrange(25,150)
-        number_2 = random.randrange(2,25)
-        calculate = (number_1 / number_2)
-        print(f"{number_1} / {number_2}")
-        control_1 = True
+        if calculation == "/":
+            number_1 = random.randrange(25,150)
+            number_2 = random.randrange(2)
+            calculate = (number_1 / number_2)
+            print(f"{number_1} / {number_2}")
+            control_1 = True
     
+    if mode == "hard":
+         
+        if calculation == "+":
+              number_1 = random.randrange(50, 500)
+              number_2 = random.randrange(110,700)
+              calculate = (number_1 + number_2)
+              print(f"{number_1} + {number_2}")
+              control_1 = True
+        
+        if calculation == "-":
+             number_1 = random.randrange(25,500)
+             number_2 = random.randrange(25,700)
+             claculate = (number_1 - number_2)
+             print(f"{number_1} - {number_2}")
+             control_1 = True
+
+        if calculation == "*":
+             number_1 = random.randrange(25,150)
+             number_2 = random.randrange(2,25)
+             calculate = (number_1 * number_2)
+             print(f"{number_1} * {number_2}")
+             control_1 = True
+
+        if calculation == "/":
+             print("You only have to write the first two numbers")
+             number_1 = random.randrange(50,500)
+             number_2 = random.randrange(2,20)
+             claculate = (number_1 / number_2)
+             print(f"{number_1} / {number_2}")   
+             control_1 = True      
+        
+    
+
     if control_1 == False:
         print("nice try")
         print("don't try this again!")
@@ -50,13 +88,17 @@ while(True):
         result_human_1 = str(result_human)  
         
         calculate_2 = str(calculate)
-             
-        if result_human == calculate  :
+
+        control_1 = False
+
+        if result_human == calculate_2  :
                 print ("correct")
                 control_1 = True
 
-        if result_human != calculate  :
-                print("wrong")
-                print(calculate)
-                control_1 = True
+        if result_human_1 != calculate_2:
+             print("wrong")
+             print(calculate_2)
+             control_1 = True
+
+        
         
