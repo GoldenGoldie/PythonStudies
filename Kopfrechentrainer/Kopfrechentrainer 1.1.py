5233c7e171cc621f21dea66323b49c215416441a
GoldenGoldie <136335517+GoldenGoldie@users.noreply.github.com>
2023-06-16 16:40:27 +0200

    fixed a bug
    when writting a letter at the first question the programm would continue

--- Kopfrechentrainer/Kopfrechentrainer 1.1.py
+++ Kopfrechentrainer/Kopfrechentrainer 1.1.py
@@ -3,70 +3,79 @@ import random
 print("what do you want to learn?")
 
 while(True):
+    control_1 = False
 
     mode = (input("Do you want the easy or hard mode?"))
-
-    calculation = (input("Show me the calculation symbol: "))
+    if mode == "hard ":
+         control_1 = True
     
-    control_1 = False
-                               
-    if calculation == "end":
-           break
-
     if mode == "easy":
-     
-        if calculation == "*":
-            number_1 = random.randrange(1,10)
-            number_2 = random.randrange(1,20)
-            calculate = (number_1* number_2)
-            print (f" { number_2 } * {number_1}")
-            control_1 = True   
+         control_1 = True
 
-        if calculation == "+":
-            number_1 = random.randrange(20,150)
-            number_2 = random.randrange(20,150)
-            calculate = (number_1 + number_2)
-            print(f"{number_1} + {number_2}")
-            control_1 = True
-
-        if calculation == "-":
-            number_1 = random.randrange(20,150)
-            number_2 = random.randrange(20,75)
-            calculate = (number_1 - number_2 )
-            print(f"{number_1} - {number_2}")
-            control_1 = True
-
-        if calculation == "/":
-            number_1 = random.randrange(25,150)
-            number_2 = random.randrange(2)
-            calculate = (number_1 / number_2)
-            print(f"{number_1} / {number_2}")
-            control_1 = True
+    if control_1 == False:
+         print ("don't do this!")
+    if control_1:
+        calculation = (input("Show me the calculation symbol: "))
     
-    if mode == "hard":
+    
+                               
+        if calculation == "end":
+           break
+    
+        if mode == "easy":
+     
+            if calculation == "*":
+                number_1 = random.randrange(1,10)
+                number_2 = random.randrange(1,20)
+                calculate = (number_1* number_2)
+                print (f" { number_2 } * {number_1}")
+                control_1 = True   
+
+            if calculation == "+":
+                number_1 = random.randrange(20,150)
+                number_2 = random.randrange(20,150)
+                calculate = (number_1 + number_2)
+                print(f"{number_1} + {number_2}")
+                control_1 = True
+
+            if calculation == "-":
+                number_1 = random.randrange(20,150)
+                number_2 = random.randrange(20,75)
+                calculate = (number_1 - number_2 )
+                print(f"{number_1} - {number_2}")
+                control_1 = True
+
+            if calculation == "/":
+                number_1 = random.randrange(25,150)
+                number_2 = random.randrange(2)
+                calculate = (number_1 / number_2)
+                print(f"{number_1} / {number_2}")
+                control_1 = True
+    
+        if mode == "hard":
          
-        if calculation == "+":
+            if calculation == "+":
               number_1 = random.randrange(50, 500)
               number_2 = random.randrange(110,700)
               calculate = (number_1 + number_2)
               print(f"{number_1} + {number_2}")
               control_1 = True
         
-        if calculation == "-":
+            if calculation == "-":
              number_1 = random.randrange(25,500)
              number_2 = random.randrange(25,700)
              claculate = (number_1 - number_2)
              print(f"{number_1} - {number_2}")
              control_1 = True
 
-        if calculation == "*":
+            if calculation == "*":
              number_1 = random.randrange(25,150)
              number_2 = random.randrange(2,25)
              calculate = (number_1 * number_2)
              print(f"{number_1} * {number_2}")
              control_1 = True
 
-        if calculation == "/":
+            if calculation == "/":
              print("You only have to write the first two numbers")
              number_1 = random.randrange(50,500)
              number_2 = random.randrange(2,20)
