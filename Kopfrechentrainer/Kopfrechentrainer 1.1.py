75e5432602c8a72a0e086f81f36e8718927d2d2a
GoldenGoldie <136335517+GoldenGoldie@users.noreply.github.com>
2023-06-21 21:03:48 +0200

    added a loops

--- Kopfrechentrainer/Kopfrechentrainer 1.1.py
+++ Kopfrechentrainer/Kopfrechentrainer 1.1.py
@@ -16,113 +16,126 @@ while(True):
          print ("don't do this!")
     if control_1:
         calculation = (input("Do you want to practice +, -, *, / or squaring: "))
+        
+        loop_count = str(input("How often do you want to practise?"))
     
-    
+        loops = "1"
                                
         if calculation == "end":
            break
-    
+        
+        loops_control = True
 
-        if mode == "easy":
+        while(loops_control):
+
+            if mode == "easy":
      
-            if calculation == "*":
-                number_1 = random.randrange(1,10)
-                number_2 = random.randrange(1,20)
-                calculate = (number_1* number_2)
-                print (f" { number_2 } * {number_1}")
-                control_1 = True   
+                if calculation == "*":
+                    number_1 = random.randrange(1,10)
+                    number_2 = random.randrange(1,20)
+                    calculate = (number_1* number_2)
+                    print (f" { number_2 } * {number_1}")
+                    control_1 = True   
 
-            if calculation == "+":
-                number_1 = random.randrange(20,150)
-                number_2 = random.randrange(20,150)
-                calculate = (number_1 + number_2)
-                print(f"{number_1} + {number_2}")
-                control_1 = True
+                if calculation == "+":
+                    number_1 = random.randrange(20,150)
+                    number_2 = random.randrange(20,150)
+                    calculate = (number_1 + number_2)
+                    print(f"{number_1} + {number_2}")
+                    control_1 = True
 
-            if calculation == "-":
-                number_1 = random.randrange(20,150)
-                number_2 = random.randrange(20,75)
-                calculate = (number_1 - number_2 )
-                print(f"{number_1} - {number_2}")
-                control_1 = True
+                if calculation == "-":
+                    number_1 = random.randrange(20,150)
+                    number_2 = random.randrange(20,75)
+                    calculate = (number_1 - number_2 )
+                    print(f"{number_1} - {number_2}")
+                    control_1 = True
 
-            if calculation == "/":
-                number_1 = random.randrange(25,150)
-                number_2 = random.randrange(2)
-                calculate = (number_1 / number_2)
-                print(f"{number_1} / {number_2}")
-                control_1 = True
+                if calculation == "/":
+                    number_1 = random.randrange(25,150)
+                    number_2 = random.randrange(2)
+                    calculate = (number_1 / number_2)
+                    print(f"{number_1} / {number_2}")
+                    control_1 = True
 
-            if calculation == "squaring":
-                number_1 = random.randrange(1,10)
-                number_2 = number_1
-                calculate = (number_1 * number_2)
-                print(f"{number_1} * {number_2}")
-                control_1 = True
+                if calculation == "squaring":
+                    number_1 = random.randrange(1,10)
+                    number_2 = number_1
+                    calculate = (number_1 * number_2)
+                    print(f"{number_1} * {number_2}")
+                    control_1 = True
     
-        if mode == "hard":
+            if mode == "hard":
          
-            if calculation == "+":
-              number_1 = random.randrange(50, 500)
-              number_2 = random.randrange(110,700)
-              calculate = (number_1 + number_2)
-              print(f"{number_1} + {number_2}")
-              control_1 = True
+                if calculation == "+":
+                    number_1 = random.randrange(50, 500)
+                    number_2 = random.randrange(110,700)
+                    calculate = (number_1 + number_2)
+                    print(f"{number_1} + {number_2}")
+                    control_1 = True
         
-            if calculation == "-":
-             number_1 = random.randrange(25,500)
-             number_2 = random.randrange(25,700)
-             claculate = (number_1 - number_2)
-             print(f"{number_1} - {number_2}")
-             control_1 = True
+                if calculation == "-":
+                 number_1 = random.randrange(25,500)
+                 number_2 = random.randrange(25,700)
+                 claculate = (number_1 - number_2)
+                 print(f"{number_1} - {number_2}")
+                 control_1 = True
 
-            if calculation == "*":
-             number_1 = random.randrange(25,150)
-             number_2 = random.randrange(2,25)
-             calculate = (number_1 * number_2)
-             print(f"{number_1} * {number_2}")
-             control_1 = True
+                if calculation == "*":
+                 number_1 = random.randrange(25,150)
+                 number_2 = random.randrange(2,25)
+                 calculate = (number_1 * number_2)
+                 print(f"{number_1} * {number_2}")
+                 control_1 = True
 
-            if calculation == "/":
-             print("You only have to write the first two numbers")
-             number_1 = random.randrange(50,500)
-             number_2 = random.randrange(2,20)
-             claculate = (number_1 / number_2)
-             print(f"{number_1} / {number_2}")   
-             control_1 = True      
+                if calculation == "/":
+                 print("You only have to write the first two numbers")
+                 number_1 = random.randrange(50,500)
+                 number_2 = random.randrange(2,20)
+                 claculate = (number_1 / number_2)
+                 print(f"{number_1} / {number_2}")   
+                 control_1 = True      
 
-            if calculation == "squaring":
-                number_1 = random.randrange(10,30)
-                number_2 = number_1 
-                calculate = (number_1 * number_2)
-                print(f"{number_1} * {number_2}")
-                control_1 = True
+                if calculation == "squaring":
+                    number_1 = random.randrange(10,30)
+                    number_2 = number_1 
+                    calculate = (number_1 * number_2)
+                    print(f"{number_1} * {number_2}")
+                    control_1 = True
         
     
 
-    if control_1 == False:
-        print("nice try")
-        print("don't try this again!")
-        control_1 = False
+            if control_1 == False:
+                print("nice try")
+                print("don't try this again!")
+                control_1 = False
     
-    if control_1:
+            if control_1:
         
-        result_human = (input("What is your result?"))      
+                result_human = (input("What is your result?"))      
 
-        result_human_1 = str(result_human)  
+                result_human_1 = str(result_human)  
         
-        calculate_2 = str(calculate)
+                calculate_2 = str(calculate)
 
-        control_1 = False
+                control_1 = False
 
-        if result_human == calculate_2  :
-                print ("correct")
-                control_1 = True
+                if result_human == calculate_2  :
+                    print ("correct")
+                    control_1 = True
 
-        if result_human_1 != calculate_2:
-             print("wrong")
-             print(calculate_2)
-             control_1 = True
+                if result_human_1 != calculate_2:
+                 print("wrong")
+                 print(calculate_2)
+                 control_1 = True
 
-        
-        
+                loop_count_2 = int(loop_count)
+
+                loops = int(loops)
+
+                loops_1 = loops + 1
+
+
+                loops = loops_1
+
+                loops_control = (loops <= loop_count_2)
