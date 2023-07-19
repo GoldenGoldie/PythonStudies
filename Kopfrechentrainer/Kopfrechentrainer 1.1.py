49b978103cee5cf0be2809a057366fceddc12bcf
GoldenGoldie <136335517+GoldenGoldie@users.noreply.github.com>
2023-07-19 19:42:02 +0200

    Played around

--- Kopfrechentrainer/Kopfrechentrainer 1.1.py
+++ Kopfrechentrainer/Kopfrechentrainer 1.1.py
@@ -1,5 +1,7 @@
 import random
 
+
+
 print("what do you want to learn?")
 
 end_control = True
---
+++ Playground/playgrond2.py
@@ -0,0 +1,13 @@
+import time
+
+Ergebniss_1 = time.time()
+
+input("Hello")
+
+Ergebniss_2 = time.time()
+
+Endergebniss = Ergebniss_2 - Ergebniss_1
+
+Endergebniss = int(Endergebniss)
+
+print(Endergebniss)
