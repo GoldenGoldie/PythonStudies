d3507d85c5006575786c47fc1320508e9c82a2d3
GoldenGoldie <136335517+GoldenGoldie@users.noreply.github.com>
2023-10-09 13:03:32 +0200

    added new things

--- app/Kopfrechentrainer app.py
+++ app/Kopfrechentrainer app.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import ttk
+import random
 
 
 root = Tk()
@@ -10,19 +11,40 @@ mainframe.grid(column=0,row=0, sticky= (N,W,E,S))
 root.columnconfigure(0,weight=1)
 root.rowconfigure(0, weight=1)
 
+def show():
+    ttk.Label(mainframe,text=f"{random.randrange(1,10)} + {random.randrange(1,10)}").grid(column=1, row= 7, sticky=(W,E))
+
+
+
+def random_number():
+
+   if mode == "easy":
+     if calculationsymbol == "-" or calculationsymbol == "+":
+         return random.randrange(1,30)
+    
+     if calculationsymbol == "*":
+         return  random.randrange(1,10)
+
+     if calculationsymbol == "/":
+          return random.randrange(2,20)
+         
+
 mode = StringVar()
 mode_entry = ttk.Entry(mainframe, width=8, textvariable=mode)
 mode_entry.grid(column=2, row=2,sticky=(W,E))
 
 calculationsymbol = StringVar()
-mode_entry = ttk.Entry(mainframe, width=8, textvariable=calculationsymbol)
-mode_entry.grid(column=2, row= 5,sticky=(W,E))
+calculationsymbol_entry = ttk.Entry(mainframe, width=8, textvariable=calculationsymbol)
+calculationsymbol_entry.grid(column=2, row= 5,sticky=(W,E))
 
 ttk.Label(mainframe, text="which mode do you want:").grid(column=1, row=1,sticky=(W,E))
-ttk.Label(mainframe, text="easy, medium, hard").grid(column=1, row=2, sticky=(W,E))
+ttk.Label(mainframe, text="you can chose these modes: easy, medium, hard    ").grid(column=1, row=2, sticky=(W,E))
 ttk.Label(mainframe, text=" calculationtyp?").grid(column=1, row=4,sticky=(W,E))
-ttk.Label(mainframe, text="+,-,*,/").grid(column=1, row=5, sticky=(W,E))
+ttk.Label(mainframe, text="you can choose these calcuationsymbols:+,-,*,/").grid(column=1, row=5, sticky=(W,E))
 ttk.Label(mainframe, text="").grid(column=1, row=3,sticky=(W,E))
+
+
+ttk.Button(mainframe, text="show calculation",command=show).grid(column=3, row=7, sticky=(W,E))
 
 
 
--- app/app Beispiel.py
+++ app/app Beispiel.py
@@ -13,6 +13,7 @@ root.title("Feet to Meters")
 #content frame
 mainframe = ttk.Frame(root, padding="3 3 12 12")
 mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 #Entry Widget
