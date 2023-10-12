2a25c72224c11bc9aacfc4dc90e6ee0a1ddc2dfe
GoldenGoldie <136335517+GoldenGoldie@users.noreply.github.com>
2023-10-12 16:06:27 +0200

    worked on the app

--- app/Kopfrechentrainer app.py
+++ app/Kopfrechentrainer app.py
@@ -6,6 +6,8 @@ import random
 root = Tk()
 root.title ("Kopfrechentreiner")
 
+
+
 mainframe = ttk.Frame(root,padding="3 3 12 12")
 mainframe.grid(column=0,row=0, sticky= (N,W,E,S))
 root.columnconfigure(0,weight=1)
@@ -13,20 +15,22 @@ root.rowconfigure(0, weight=1)
 
 def show():
     ttk.Label(mainframe,text=f"{random.randrange(1,10)} + {random.randrange(1,10)}").grid(column=1, row= 7, sticky=(W,E))
+    ttk.Label(mainframe,text=mode).grid(column=2, row= 14, sticky=(W,E))
+def random_number_gen():
+    try:
+        calculationtyp = str(calculationsymbol.get())
+        modes = str(mode)
 
 
 
-def random_number():
+    except ValueError:
+        pass
 
-   if mode == "easy":
-     if calculationsymbol == "-" or calculationsymbol == "+":
-         return random.randrange(1,30)
+def chechbox_check():
+    ttk.Label(mainframe,text="it works").grid(column=1, row=15, sticky=(W,E))
     
-     if calculationsymbol == "*":
-         return  random.randrange(1,10)
 
-     if calculationsymbol == "/":
-          return random.randrange(2,20)
+
          
 
 mode = StringVar()
@@ -44,7 +48,16 @@ ttk.Label(mainframe, text="you can choose these calcuationsymbols:+,-,*,/").grid
 ttk.Label(mainframe, text="").grid(column=1, row=3,sticky=(W,E))
 
 
+
+def trystuff():
+    ttk.Label(mainframe, text=calculationsymbol).grid(column=1, row=9, sticky=(W,E))
+
+
 ttk.Button(mainframe, text="show calculation",command=show).grid(column=3, row=7, sticky=(W,E))
+ttk.Button(mainframe, text="try", command=random_number_gen).grid(column=2, row=9, sticky=(W,E))
+
+x = ttk.Checkbutton(root, text= "easy",command=chechbox_check,onvalue="modes",offvalue= "other modes").grid(column=1, row= 13,sticky=(W,E))
+
 
 
 
