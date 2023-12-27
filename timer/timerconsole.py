8c078ca9251204c14c48674404b0a881aa9670ef
GoldenGoldie <136335517+GoldenGoldie@users.noreply.github.com>
2023-12-27 16:01:53 +0100

    worked on the timer

--- timer/timerconsole.py
+++ timer/timerconsole.py
@@ -2,11 +2,52 @@ import time
 
 count = True
 
-timeaumount = int(input("how long should the timer go (in seconds)?"))
+def CheckIfIsDigit(I):
+    if I.isdigit():
+        return int(I)
+    else:
+        return "no number"
+    
+def timesymbol(I):
+    if I == "sec" or I == "s":
+        return 1
+    if I == "min" or I == "m":
+        return 60 
+    if I == "hour" or I == "h":
+        return 3600
+
+def Checkiftimesymbol(I):
+    I = timerdurationsymbol
+    Y = CheckIfIsDigit(I)
+
+    if Y == "no number":
+        I = timesymbol(I)
+        return I
+
+
+    
+    
+input = input("how long should the timer go ?")
+
+timeramount = input.split()
+
+timernumber = timeramount[0]
+
+timernumber = CheckIfIsDigit(timernumber)
+
+timerdurationsymbol = timeramount[1]
+
+timerdurationsymbol = Checkiftimesymbol(timerdurationsymbol)
+
+print(timernumber)
 
 time_start = int(time.time())
 
-time_end = time_start + timeaumount
+timernumber = timernumber * timerdurationsymbol
+
+time_end = time_start + timernumber
+
+
 
 while count:
     time_duration = int(time.time())
