398a2ca271b58bb1fa63847079e4be34f1b04dc7
GoldenGoldie <136335517+GoldenGoldie@users.noreply.github.com>
2024-02-12 13:02:09 +0100

    made it a lot better

---
+++ Learnplaner/Learnplaner.py
@@ -0,0 +1,53 @@
+def checknumber(i):
+    if i.isdigit():
+        return float(i)
+    else:
+        return "no number"
+    
+
+daysleft = input("how many days are left until the exam")
+
+time_day = input("how much time do you want to invest (in hours)?")
+
+daysleft = checknumber(daysleft)
+
+time_day = checknumber(time_day)
+
+if daysleft != "no number" or time_day != "no number":
+    timedayminutes = time_day * 60
+else:
+    timedayminutes = "no number"
+    
+counter = 0
+
+easy_task = 5
+
+mid_task = 30
+
+hard_task = 45
+
+
+while True:
+    
+    if timedayminutes == "no number":
+        break
+
+
+    amount_hard_task = int(timedayminutes / hard_task)
+    timeday = timedayminutes - (hard_task * amount_hard_task)
+
+    amount_mid_task = int(timeday / mid_task)
+    timeday = timeday - (mid_task * amount_mid_task)
+
+    amount_easy_task = int(timeday / easy_task)
+    timeday = time_day - (easy_task *amount_easy_task)
+
+    counter += 1
+
+    print(f"day{counter}")
+    print(f"{amount_hard_task}hard tasks")
+    print(f"{amount_mid_task} medium tasks")
+    print(f"{amount_easy_task} easy tasks")
+
+    if counter == daysleft:
+        break
